2df51194ed8bc3445c62955a7ebeb09d6a28b346
RohSeongHoon <scubarsh@naver.com>
2022-04-10 17:20:33 +0900

    커넥션을 인터페이스로 생성해서 확장성을 높이고 사용하는 쪽에서는 구체적인 로직을 알아야 할 필요성을 없앰 구현클래스로 자요롭게 확장 변경을 할 수 있도록 만듦

---
+++ src/main/java/com/study/springboot_base/connection/NConnectionMaker.java
@@ -0,0 +1,14 @@
+package com.study.springboot_base.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NConnectionMaker implements ConnectionMaker {
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/testdb", "root", "qsc20215");
+        return connection;
+    }
+}
